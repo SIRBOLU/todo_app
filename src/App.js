b9884811cf0915a024d65d1e82d3fd9e70d3ca9b
Sirbolu <peterbolu@yahoo.com>
2025-03-18 15:51:53 +0100

    first commmit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import { TodoWrapper } from "./components/TodoWrapper";
+// import TodoForm from "./components/TodoForm";
+import "./App.css";
+import { TodoWrapper } from "./components/todoWrapper";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/todoWrapper.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+
+import { v4 as uuidv4 } from "uuid";
+import { TodoForm } from "./todoForm";
+import { Todo } from "./todo";
+import { EditTodoForm } from "./EditTodoForm";
+uuidv4();
+
+export const TodoWrapper = () => {
+  // const [todos, setTodos] = useState([]);
+
+  // Initialize state from localStorage or empty array
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem("todos");
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
+
+  // Save to localStorage whenever todos changes
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+    console.log(todos);
+  };
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.lsEditing } : todo
+      )
+    );
+  };
+
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+  return (
+    <div className="TodoWrapper">
+      <h1>Tasks to be caried out!</h1>
+      <TodoForm addTodo={addTodo} />
+      {todos.map((todo, index) =>
+        todo.isEditing ? (
+          <EditTodoForm editTodo={editTask} task={todo} />
+        ) : (
+          <Todo
+            task={todo}
+            key={index}
+            toggleComplete={toggleComplete}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+          />
+        )
+      )}
+    </div>
+  );
+};
