756ea66c3e01a94ae07a55c45cba5def6c6dc680
Sirbolu <peterbolu@yahoo.com>
2025-03-19 15:57:32 +0100

    mobile

--- src/components/todo.js
+++ src/components/todo.js
@@ -5,19 +5,21 @@ import React from "react";
 
 export const Todo = ({ task, toggleComplete, deleteTodo, editTodo }) => {
   return (
-    <div className="Todo">
-      <p
-        onClick={() => toggleComplete(task.id)}
-        className={`'${task.completed ? "completed" : ""}`}
-      >
-        {task.task}
-      </p>
-      <div>
-        <FontAwesomeIcon
-          icon={faPenToSquare}
-          onClick={() => editTodo(task.id)}
-        />
-        <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)} />
+    <div className="cover">
+      <div className="Todo">
+        <p
+          onClick={() => toggleComplete(task.id)}
+          className={`'${task.completed ? "completed" : ""}`}
+        >
+          {task.task}
+        </p>
+        <div>
+          <FontAwesomeIcon
+            icon={faPenToSquare}
+            onClick={() => editTodo(task.id)}
+          />
+          <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)} />
+        </div>
       </div>
     </div>
   );
